e23cf51c22bd76a733f36f8a7b28a6935376e443
Ray Villalobos <ray@planetoftheweb.com>
2019-10-27 12:36:27 -0700

    01_05 Lifecycle Methods

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,14 @@
 <script>
+  import { onMount } from "svelte";
   import ArtistList from "./ArtistList.svelte";
   import ArtistSearch from "./ArtistSearch.svelte";
   let searchTerm = "Barot Bellingham";
-  let artists = [
-    {
-      name: "Barot Bellingham",
-      src: "https://lil-cdn.com/2824055/Barot_Bellingham_tn.jpg",
-      reknown: "Royal Academy of Painting and Sculpture"
-    },
-    {
-      name: "Jonathan G. Ferrar II",
-      src: "https://lil-cdn.com/2824055/Jonathan_Ferrar_tn.jpg",
-      reknown: "Artist to Watch in 2012"
-    }
-  ];
+  let artists = [];
+
+  onMount(async () => {
+    const res = await fetch(`data.json`);
+    artists = await res.json();
+  });
 </script>
 
 <style global lang="scss">
--- src/ArtistList.svelte
+++ src/ArtistList.svelte
@@ -16,14 +16,23 @@
   }
 </style>
 
-{#each artists as { name, src, reknown }}
-  <div
-    class="list-group-item d-flex w-100 list-group-item-action
-    align-items-center">
-    <img class="rounded mr-3 d-block" {src} alt={`Photo of ${name}`} />
-    <div>
-      <h4 class="mb-0">{name}</h4>
-      <p class="text-muted mb-0">{reknown}</p>
+<div class="row justify-content-center">
+  <div class="col-11 col-md-7 col-lg-5">
+    <div class="list-group">
+      {#each artists as { name, src, reknown }}
+        <div
+          class="list-group-item d-flex w-100 list-group-item-action
+          align-items-center">
+          <img class="rounded mr-3 d-block" {src} alt={`Photo of ${name}`} />
+          <div>
+            <h4 class="mb-0">{name}</h4>
+            <p class="text-muted mb-0">{reknown}</p>
+          </div>
+        </div>
+      {:else}
+        <p>No data</p>
+      {/each}
+
     </div>
   </div>
-{/each}
+</div>
