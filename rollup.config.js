415c304aa672ef9aa9bc42c7366829d065e77e8d
Ray Villalobos <ray@planetoftheweb.com>
2019-10-26 18:31:05 -0700

    01_02e sass modules

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import commonjs from 'rollup-plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import rollup_start_dev from './rollup_start_dev';
+import autoPreprocess from 'svelte-preprocess';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -19,6 +20,7 @@ export default {
 		svelte({
 			// enable run-time checks when not in production
 			dev: !production,
+			preprocess: autoPreprocess(),
 			// we'll extract any component CSS out into
 			// a separate file — better for performance
 			css: css => {
--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,23 @@
 <script>
-	export let name;
+  export let name;
 </script>
 
-<style>
-	h1 {
-		color: purple;
-	}
+<style lang="scss">
+  $primary: purple;
+
+  @import "../node_modules/bootstrap/scss/bootstrap.scss";
+
+  h1 {
+    color: $primary;
+  }
 </style>
 
-<h1>Hello {name}!</h1>
+<div class="container">
+  <h1>Hello {name}!</h1>
+  <p class="text-primary">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam deserunt eos
+    quam reprehenderit porro, dolor nulla veritatis iure, voluptas repellendus
+    consequuntur fugit illum similique? Ab obcaecati doloribus distinctio iure?
+    Dolor.
+  </p>
+</div>
